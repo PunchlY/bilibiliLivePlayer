72745dd33fe04507334e740171dd1100d18b0b95
PunchlY <1442440778@qq.com>
2022-03-06 21:58:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Hls = window.Hls
-var url = 'https://d1--cn-gotcha03.bilivideo.com/live-bvc/346915/live_509069409_67641662.flv?cdn=cn-gotcha03&expires=1646578180&len=0&oi=2634039694&pt=web&qn=10000&trid=1000ae6c26c5431a42d09123c85ee80e9fa3&sigparams=cdn,expires,len,oi,pt,qn,trid&sign=fd0d73a50d12704cd55e45538e7e1ced&ptype=0&src=4&sl=1&sk=30ca2b1b36442cde4c50e29e44590c13&order=4'
+var url = 'http://103.199.161.254/Content/bbcworld/Live/Channel(BBCworld)/index.m3u8'
 if (Hls.isSupported()) {
   var hls = new Hls()
   hls.loadSource(url)
