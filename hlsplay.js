1cdc702063d6d6ff1438d426e903f5f689fcebcf
PunchlY <1442440778@qq.com>
2022-03-19 21:29:00 +0800

    Create hlsplay.js

---
+++ hlsplay.js
@@ -0,0 +1,26 @@
+//by https://github.com/video-dev/hls.js/
+
+var newscript = document.createElement('script');
+newscript.src = 'https://cdn.jsdelivr.net/npm/hls.js@1.1.5/dist/hls.min.js';
+document.head.appendChild(newscript);
+
+var hls;
+
+function Hlsplay(video, videoSrc) {
+  if (Hls.isSupported()) {
+    hls = new Hls();
+    hls.loadSource(videoSrc);
+    hls.attachMedia(video);
+  }
+  else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = videoSrc;
+  }
+}
+
+function Hlsoff() {
+  try {
+    hls.stopLoad();
+    hls.destory();
+  } catch (e) {
+  }
+}
