748e78ba5a63f4ee74851c56bb0d9521a8253d59
PunchlY <1442440778@qq.com>
2022-03-19 22:43:33 +0800

    Update hlsplay.js

--- hlsplay.js
+++ hlsplay.js
@@ -5,10 +5,11 @@ newscript.src = 'https://cdn.jsdelivr.net/npm/hls.js@1.1.5/dist/hls.min.js';
 document.head.appendChild(newscript);
 
 var hls;
+var config = {};
 
 function Hlsplay(video, videoSrc) {
   if (Hls.isSupported()) {
-    hls = new Hls();
+    hls = new Hls(config);
     hls.loadSource(videoSrc);
     hls.attachMedia(video);
   }
