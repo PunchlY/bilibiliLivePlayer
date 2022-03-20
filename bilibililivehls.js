c7b045b4d95fd3190ba6426661dd7caaefb77894
PunchlY <1442440778@qq.com>
2022-03-20 17:11:29 +0800

    --

--- bilibililivehls.js
+++ bilibililivehls.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BiliBili直播-白嫖
-// @namespace    https://github.com/PunchlY/hlsplayer
-// @version      0.2
+// @namespace    http://tampermonkey.net/
+// @version      0.1
 // @description  直播白嫖
 // @author       punchly
 // @match        https://punchly.github.io/hlsplayer/
@@ -9,18 +9,21 @@
 // @connect      *
 // @grant        GM_xmlhttpRequest
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @homepage     https://punchly.github.io/
 // ==/UserScript==
 
 (function () {
-    $('body').prepend("<div id='search'>");
-    $('#search').prepend("<input name='search' type='button' id='display' value='/\\'>");
+    $('#search').prepend("<input name='search' type='button' id='display' value='close'>");
     $(function () {
+        $("#live").hide();
         $('body').on('click', "#display", function () {
             $("#search_response").toggle();
+            $("#live").toggle();
+            $("#live").toggle();
             if ($('#search_response').css('display') == 'none') {
-                $("#display").attr("value", '\\/');
+                $("#display").attr("value", 'open');
             } else {
-                $("#display").attr("value", '/\\');
+                $("#display").attr("value", 'close');
             }
         });
     });
@@ -32,8 +35,8 @@
     });
     $('#search').append("<div id='search_request'>");
     $('#search_request').append("<input name='search' type='text' id='key' placeholder='search'>");
-    $('#search_request').append("<input name='search' type='radio' id='room' value='live_room' checked='checked'>room");
-    $('#search_request').append("<input name='search' type='radio' id='user' value='live_user'>user");
+    $('#search_request').append("<input name='search' type='radio' value='live_room' checked='checked'>room");
+    $('#search_request').append("<input name='search' type='radio' value='live_user'>user");
     $('#search_request').append("<input name='search' type='number' id='page' placeholder='page' value='1' min='1' max='1'>");
     $(function () {
         $('#key').on('keypress', function (event) {
@@ -54,34 +57,36 @@
     $('#search_request').append("<input name='search' type='button' id='random' value='random'>");
     $(function () {
         $('body').on('click', "#random", function () {
+            if ($('#search_response').css('display') == 'none') $('#display').click();
             random();
         });
     });
     random();
-    $('body').append("<div id='out'>");
+    $('#search').append("<div id='out'>");
 })();
 
 function search(key, type, page) {
     if (key == '' || type == '' || page == '') return false;
+    if ($('#search_response').css('display') == 'none') $('#display').click();
     $("#search_response").empty();
-    $('#search_response').append("<div id='islive'>")
-    $('#search_response').append("<div id='dislive'>")
+    $('#search_response').append("<ul id='islive'>");
+    $('#search_response').append("<ul id='dislive'>");
     GM_xmlhttpRequest({
-        method: "get",
+        method: 'get',
         url: `http://api.bilibili.com/x/web-interface/search/type?search_type=${type}&keyword=${key}&page=${page}`,
         onload: function (res) {
             let json = JSON.parse(res.responseText);
             let data = json.data;
-            $("#page").attr("max", data.numPages)
+            $('#page').attr('max', data.numPages)
             data.result.forEach(function (element) {
                 if (element.live_status) {
-                    $('#islive').append('<p>');
-                    $('#islive').append(`<input name='play' type='radio' id=${element.roomid} value=${element.roomid}>`);
-                    $('#islive').append(`<small><b>[live]</b></small> ${element.title}-<small>[${element.tags}]</small> <sub><b>${element.uname}</b> <sup>[${element.cate_name}]</sup></sub>`);
+                    $('#islive').append(`<li>
+                    <button class='play' value=${element.roomid}>
+                    <small><b>[live]</b></small> ${element.title}-<small>[${element.tags}]</small> <sub><b>${element.uname}</b> <sup>[${element.cate_name}]</sup></sub>`);
                 }
                 else {
-                    $('#dislive').append('<p>');
-                    $('#dislive').append(`<small><b>[X]</b></small>${element.uname} ${element.cate_name}`);
+                    $('#dislive').append(`<li>
+                                         <small><b>[X]</b></small>${element.uname} ${element.cate_name}`);
                 }
             });
         }
@@ -90,7 +95,7 @@ function search(key, type, page) {
 
 function random() {
     $("#search_response").empty();
-    $('#search_response').append("<div id='getRecommendList'>")
+    $('#search_response').append("<ul id='getRecommendList'>");
     $('#getRecommendList').append('<p><b>Recommend List:</b>');
     GM_xmlhttpRequest({
         method: "get",
@@ -99,13 +104,13 @@ function random() {
             let json = JSON.parse(res.responseText);
             let data = json.data;
             data.list.forEach(function (element) {
-                $('#getRecommendList').append('<p>');
-                $('#getRecommendList').append(`<input name='play' type='radio' id=${element.roomid} value=${element.roomid}>`);
-                $('#getRecommendList').append(`<small><b>[live]</b></small>${element.roomname}<sub><b>${element.nickname}</b></sub>`);
+                $('#getRecommendList').append(`<li>
+                <button class='play' value=${element.roomid}>
+                <small><b>[live]</b></small>${element.roomname}<sub><b>${element.nickname}</b></sub>`);
             });
         }
     });
-    $('#search_response').append("<div id='getList'>")
+    $('#search_response').append("<ul id='getList'>");
     $('#getList').append('<p><b>platform=web:</b>');
     GM_xmlhttpRequest({
         method: "get",
@@ -114,18 +119,18 @@ function random() {
             let json = JSON.parse(res.responseText);
             let data = json.data;
             data.recommend_room_list.forEach(function (element) {
-                $('#getList').append('<p>');
-                $('#getList').append(`<input name='play' type='radio' id=${element.roomid} value=${element.roomid}>`);
-                $('#getList').append(`<small><b>[live]</b></small>${element.title} <sub><b>${element.uname}</b> <sup>${element.area_v2_parent_name}-${element.area_v2_name}</sup></sub>`);
+                $('#getList').append(`<li>
+                <button class='play' value=${element.roomid}>
+                <small><b>[live]</b></small>${element.title} <sub><b>${element.uname}</b> <sup>${element.area_v2_parent_name}-${element.area_v2_name}</sup></sub>`);
             });
         }
     });
 }
 $(function () {
-    $('body').on('click', ":radio[name=play]", function () {
+    $('body').on('click', ".play", function () {
         $('#display').click();
-        $("#out").empty();
-        let room_id = $(':radio[name="play"]:checked').val();
+        $('#out').empty();
+        let room_id = $(this).val();
         //chat(room_id);
         GM_xmlhttpRequest({
             method: "get",
@@ -133,17 +138,15 @@ $(function () {
             onload: function (res) {
                 let json = JSON.parse(res.responseText);
                 let data = json.data;
-                $('#out').append("<div id='qn'>")
-                $('#out').append("<div id='line'>")
+                $('#out').append('<ol id="qn">');
+                $('#out').append("<ol id='line'>");
                 data.playurl_info.playurl.stream[0].format[0].codec[0].accept_qn.forEach(function (accept_qn) {
-                    $('#qn').append('<p>');
-                    $('#qn').append(`<input name='qn' type='radio' id=${accept_qn} value=${accept_qn}>${accept_qn}`);
+                    $('#qn').append(`<li><button class='qn' value=${accept_qn}>${accept_qn}`);
                 });
             }
         });
-        $('body').on('click', ":radio[name=qn]", function () {
-            $("#line").empty();
-            geturl(room_id, $(':radio[name="qn"]:checked').val());
+        $('body').on('click', '.qn', function () {
+            geturl(room_id, $(this).val());
         });
     });
     function geturl(room_id, qn) {
@@ -157,14 +160,16 @@ $(function () {
                     if (stream_info.format[0].format_name == 'ts') {
                         let base_url = stream_info.format.slice(-1)[0].codec[0].base_url;
                         let url_info = stream_info.format.slice(-1)[0].codec[0].url_info;
+                        $("#line").empty();
                         url_info.forEach(function (info, index) {
-                            $('#line').append('<p>');
-                            $('#line').append(`<input name='line' type='radio' id='line_${index}' value=${info.host}${base_url}${info.extra}>line[${index}]`);
+                            let url = `${info.host}${base_url}${info.extra}`;
+                            $('#line').append(`<li><button class='line' value=${url}>line[${index}]`);
                         });
                     }
                 });
-                $('body').on('click', ":radio[name=line]", function () {
-                    console.log($(':radio[name="line"]:checked').val());
+                $('body').on('click', '.line', function () {
+                    play($(this).val());
+                    console.log($(this).val());
                 });
             }
         });
